178e08fb5119c4bb601e18d3aff6b8fde942ab2a
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-09-13 13:13:08 +0300

    created more Reply methods

--- GoReview/src/main/java/com/example/GoReview/services/UserService.java
+++ GoReview/src/main/java/com/example/GoReview/services/UserService.java
@@ -1,5 +1,6 @@
 package com.example.GoReview.services;
 
+import com.example.GoReview.models.Reply;
 import com.example.GoReview.models.User;
 import com.example.GoReview.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,13 @@ public class UserService {
         return user;
     }
 
-    
+    public Reply processUser(User user) {
+        if(getAllUsers().contains(user.getUsername()) || getAllUsers().contains(user.getEmail())) {
+            return new Reply("This user already exists, please try again.");
+        } else {
+            saveUser(user);
+            return new Reply("User created successfully!");
+        }
+    }
 
 }
