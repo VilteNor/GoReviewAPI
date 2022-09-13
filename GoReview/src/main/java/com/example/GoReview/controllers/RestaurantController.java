82c5ba32994babb72f05a55d4368c7e0f9305c35
Veron Sundaram <veronsundaram13@gmail.com>
2022-09-13 12:58:49 +0100

    added restaurant controller

--- GoReview/src/main/java/com/example/GoReview/controllers/RestaurantController.java
+++ GoReview/src/main/java/com/example/GoReview/controllers/RestaurantController.java
@@ -1,4 +1,42 @@
 package com.example.GoReview.controllers;
 
+import com.example.GoReview.models.Restaurant;
+import com.example.GoReview.services.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/Restaurants")
 public class RestaurantController {
+
+    @Autowired
+    RestaurantService restaurantService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Restaurant>> getAllRestaurants(){
+        List<Restaurant> restaurants = restaurantService.getAllRestaurants();
+        return new ResponseEntity<>(restaurants, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Restaurant> getRestaurantsById(@PathVariable long id){
+        Optional<Restaurant> restaurant = restaurantService.getRestaurantById(id);
+        if (restaurant.isPresent()){
+            return new ResponseEntity<>(restaurant.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Restaurant> addNewRestaurant(@RequestBody Restaurant restaurant){
+        Restaurant savedRestaurant = restaurantService.saveRestaurant(restaurant);
+        return  new ResponseEntity<>(savedRestaurant, HttpStatus.OK);
+    }
 }
